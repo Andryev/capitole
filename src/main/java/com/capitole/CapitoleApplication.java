e8bb3cc41fd7453432d5fb2abc313e1bc4f6f28b
Andryev Lemes <andryev16@gmail.com>
2022-09-25 16:27:33 +0100

    Initial project

---
+++ src/main/java/com/capitole/CapitoleApplication.java
@@ -0,0 +1,13 @@
+package com.capitole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitoleApplication.class, args);
+	}
+
+}
